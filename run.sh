b2dfaa6917f0d9360c2fdf2ed6cb471e99362c25
nikiv-coder <n-e-d@mail.ru>
2024-07-19 14:48:10 +0300

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 export MAVEN_OPTS="$MAVEN_OPTS -javaagent:/home/user/.m2/repository/org/jacoco/org.jacoco.agent/0.8.10/org.jacoco.agent-0.8.10-runtime.jar=destfile=target/jacoco.exec,output=file,append=false,dumponexit=true" 
-mvn jqf:repro -Dclass=ru.algont.exampleclass.ExampleClassFuzzTest -Dmethod=testFuzzDimMe -Dinput=target/fuzz-results/ru.algont.exampleclass.ExampleClassFuzzTest/testFuzzDimMe/corpus/ -DprintArgs
-#echo "$MAVEN_OPTS"
+mvn jqf:repro -Dclass=com.org.exampleclass.ExampleClassFuzzTest -Dmethod=testFuzzDimMe -Dinput=target/fuzz-results/com.org.exampleclass.ExampleClassFuzzTest/testFuzzDimMe/corpus/ -DprintArgs
+
